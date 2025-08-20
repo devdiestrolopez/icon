ad9e3077b698d9986c5efee485982aec110bec29
Carlos Diestro <48551464+devdiestrolopez@users.noreply.github.com>
2025-08-20 16:00:20 -0400

    build: set up maven publishing configuration

--- icon-compose-android/build.gradle.kts
+++ icon-compose-android/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.icon.android.library.compose)
     alias(libs.plugins.icon.android.library)
+    id("maven-publish")
+    signing
 }
 
 android {
@@ -13,6 +15,58 @@ android {
             consumerProguardFiles("consumer-rules.pro")
         }
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "io.github.devdiestrolopez.icon.compose"
+            artifactId = "icon-compose-android"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            pom {
+                name = "Icon Compose Android"
+                description = "Jetpack Compose library that provides a single, unified Icon composable that works with both standard drawable " +
+                        "resources and Compose ImageVectors by using the IconResource sealed interface from the icon-core-android library."
+                inceptionYear = "2025"
+                url = "https://github.com/devdiestrolopez/icon"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "devdiestrolopez"
+                        name = "Carlos Diestro Lopez"
+                        email = "devdiestrolopez@gmail.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/devdiestrolopez/icon.git"
+                    developerConnection = "scm:git:ssh://github.com/devdiestrolopez/icon.git"
+                    url = "https://github.com/devdiestrolopez/icon"
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
 
 dependencies {
--- icon-core-android/build.gradle.kts
+++ icon-core-android/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.icon.android.library.compose)
     alias(libs.plugins.icon.android.library)
+    id("maven-publish")
+    signing
 }
 
 android {
@@ -13,6 +15,57 @@ android {
             consumerProguardFiles("consumer-rules.pro")
         }
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "io.github.devdiestrolopez.icon.core"
+            artifactId = "icon-core-android"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            pom {
+                name = "Icon Core Android"
+                description = "Foundational library that provides a type-safe, unified API for handling drawable vectors and vector images resources."
+                inceptionYear = "2025"
+                url = "https://github.com/devdiestrolopez/icon"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "devdiestrolopez"
+                        name = "Carlos Diestro Lopez"
+                        email = "devdiestrolopez@gmail.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/devdiestrolopez/icon.git"
+                    developerConnection = "scm:git:ssh://github.com/devdiestrolopez/icon.git"
+                    url = "https://github.com/devdiestrolopez/icon"
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
 
 dependencies {
